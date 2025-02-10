7be47c004cccf3fb4b3dac81f5b3155ee34a422c
VdovinKNT <anvdovin@edu.hse.ru>
2025-02-10 19:07:15 +0300

    new unite and tests

--- lib_dsu/dsu.h
+++ lib_dsu/dsu.h
@@ -21,8 +21,8 @@ public:
 };
 
 DSU::DSU(int size) : _size(size) {
-    _parent = new int[size];
-    _rank = new int[size];
+    _parent = new int[size + 1]; 
+    _rank = new int[size + 1];   
 
     clear();
 }
@@ -33,26 +33,25 @@ DSU::~DSU() {
 }
 
 void DSU::make_set(int elem) {
-    if (elem <= 0 || elem > _size) {
+    if (elem <= 0 || elem > _size) { 
         throw std::logic_error("Input Error: Element out of range.");
     }
-    _parent[elem - 1] = elem;
-    _rank[elem - 1] = 0;
+    _parent[elem] = elem; 
+    _rank[elem] = 0;    
 }
 
 int DSU::find(int elem) {
-    if (elem <= 0 || elem > _size) {
+    if (elem <= 0 || elem > _size) { 
         throw std::logic_error("Input Error: Element out of range.");
     }
-    elem--;
     if (_parent[elem] != elem) {
-        _parent[elem] = find(_parent[elem]); 
+        _parent[elem] = find(_parent[elem]);
     }
     return _parent[elem];
 }
 
 void DSU::unite(int first, int second) {
-    if (first <= 0 || first > _size || second <= 0 || second > _size) {
+    if (first <= 0 || first > _size || second <= 0 || second > _size) { 
         throw std::logic_error("Input Error: Element out of range.");
     }
 
@@ -60,18 +59,24 @@ void DSU::unite(int first, int second) {
     int root_second = find(second);
 
     if (root_first != root_second) {
-        _parent[root_second - 1] = root_first;
+        if (_rank[root_first] < _rank[root_second]) {
+            _parent[root_first] = root_second;
+        }
+        else if (_rank[root_first] > _rank[root_second]) { 
+            _parent[root_second] = root_first;
+        }
+        else {
+            _parent[root_second] = root_first;
+            _rank[root_first]++; 
+        }
     }
 }
 
 void DSU::clear() {
-    for (int i = 0; i < _size; i++) {
-        _parent[i] = i + 1;
+    for (int i = 1; i <= _size; i++) { 
+        _parent[i] = i;
         _rank[i] = 0;
     }
 }
 
-
-
 #endif // DSU_H
-
